4baf8157f3a5dc8d126eccbf42572a0e63caee5c
Marcela Silvério <marcelapsilverio@gmail.com>
2023-05-07 18:40:28 -0300

    changing container commands

--- container/startup.sh
+++ container/startup.sh
@@ -1,10 +1,8 @@
 change_permissions() {
-    cd /code
-    chmod 777 -R $1
+    cd /code && chmod 777 -R $1
 }
 
 change_permissions api
 change_permissions front
 
-cd /code/api
-java -jar target/api-0.0.1-SNAPSHOT.jar
+cd /code/api && java -jar target/api-0.0.1-SNAPSHOT.jar
